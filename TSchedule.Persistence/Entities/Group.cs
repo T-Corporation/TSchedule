455c7c807f6bab65c4c1b9b419e76c48b6b35c6d
va.sokolov <sokolov_vadim95@bk.ru>
2024-10-19 11:25:43 +0300

    Specialization, Group

--- TSchedule.Persistence/Entities/Group.cs
+++ TSchedule.Persistence/Entities/Group.cs
@@ -6,5 +6,13 @@ public class Group
 {
     [Key] public int Id { get; set; }
 
+    [Required(AllowEmptyStrings = false)]
+    [StringLength(20, MinimumLength = 3)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(AllowEmptyStrings = false)]
+    [StringLength(20, MinimumLength = 3)]
+    public string Specialization { get; set; } = string.Empty;
+
     // ...
 }
---
+++ TSchedule.Persistence/Entities/Specialization.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TSchedule.Persistence.Entities
+{
+    class Specialization
+    {
+        [Key] public string Code { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(20, MinimumLength = 3)]
+        public string Reduction { get; set; } = string.Empty;
+
+    }
+}
